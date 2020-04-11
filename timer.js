99b15f150f5aabcd7083953bcc2f05a1f53224d5
vishnu0179 <vjvj140@gmail.com>
2020-04-11 19:04:40 +0530

    First Commit

---
+++ timer.js
@@ -0,0 +1,31 @@
+var timer = $('.timer'),
+
+
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    for(var i=0;i<timer.size();i++)
+    {
+        timer[i].textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    }
+
+    
+
+    setTimeout(add, 1000);
+    
+}
+ setTimeout(add, 1000);
+
+
+
